d90fa829c15c95a0907b342d0ad23f95f1e0d0f8
Itodo-S <itodosimonitodo1@gmail.com>
2024-10-11 04:33:12 +0100

    layout ui implementation

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,10 +1,19 @@
+// COMPONENTS
+import ConnectWalletButton from "../components/customButtons/ConnectWalletButton";
 
 const NavBar = () => {
   return (
-    <div className="w-full border h-20">
-      
+    <div className="w-full border h-20 flex items-center justify-between px-6">
+      <div>
+        <p className="text-2xl font-bold text-gray-400 underline">
+          pay<span className="text-3xl text-cyan-400 animate-pulse">N</span>voice
+        </p>
+      </div>
+      <div>
+        <ConnectWalletButton />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/layout/SideBar.jsx
+++ src/layout/SideBar.jsx
@@ -1,10 +1,57 @@
+import { useState, useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+
+// ASSETS
+import {
+  FaFileInvoiceDollar,
+  FaUsers,
+  FaCog,
+  FaChartLine,
+} from "react-icons/fa";
+
+// Dummy nav items
+const navItems = [
+  {
+    id: 1,
+    name: "Create Invoice",
+    path: "/invoice/test",
+    icon: FaFileInvoiceDollar,
+  },
+  { id: 2, name: "Customers", path: "/customers", icon: FaUsers },
+  { id: 3, name: "Reports", path: "/reports", icon: FaChartLine },
+  { id: 4, name: "Settings", path: "/settings", icon: FaCog },
+];
+
 const SideBar = () => {
-    return (
-      <div className="w-[260px] border h-full">
-        {/* Sidebar content */}
+  const location = useLocation();
+  const [activeNav, setActiveNav] = useState("");
+
+  // Update the active path on route change
+  useEffect(() => {
+    setActiveNav(location.pathname);
+  }, [location]);
+
+  return (
+    <div className="w-[260px] border h-full py-6">
+      <div className="w-full flex flex-col gap-2">
+        {navItems.map((item) => (
+          <NavLink
+            key={item.id}
+            to={item.path}
+            className={`w-full h-10 cursor-pointer border-x-[8px] border-transparent flex items-center gap-5 px-3 ${
+              activeNav === item.path
+                ? "bg-cyan-100 border-cyan-400 text-cyan-700"
+                : "hover:border-cyan-400 hover:bg-cyan-100 text-gray-400 hover:text-cyan-700"
+            }`}
+            onClick={() => setActiveNav(item.path)}
+          >
+            <item.icon className={`text-xl`} />
+            <p>{item.name}</p>
+          </NavLink>
+        ))}
       </div>
-    );
-  }
-  
-  export default SideBar;
-  
+    </div>
+  );
+};
+
+export default SideBar;
