5ffabb08250d32ac8459fb88ab808f4481e3227d
Akanimorex <akanimoime2016@gmail.com>
2024-10-12 02:27:33 +0100

    animated the landing page

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -3,50 +3,54 @@ import { Link } from "react-router-dom";
 import avatarAnisha from "../../assets/images/avatar-anisha.png";
 import avatarAli from "../../assets/images/avatar-ali.png";
 import avatarRichard from "../../assets/images/avatar-richard.png";
+import unnamed from "../../assets/images/unnamed.jpg";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../utils/motion";
 
 const Testimonial = () => {
+  const testimonialArray = [
+    {
+      name:"Anisha Li",
+      image:unnamed,
+      content:"Using the platform has completely changed how we handle client payments.The escrow feature ensures that we’re always paid on time, and the automation saves us hours every month."
+    },
+    {
+      name:"Ali Bravo",
+      image:unnamed,
+      content:"We’ve integrated the platform into our business,and it’s made tracking payments and milestones easier than ever.Plus, the blockchain transparency gives us peace of mind."
+    },
+    {
+      name:"Akanimo Rex",
+      image:unnamed,
+      content:"As a project manager, coordinating payments across different suppliers used to be a nightmare.Now, payments are split and distributed automatically, making everything run smoother.."
+    },
+  ]
   return (
     <section id="testimonials">
       {/* Container to heading and testm blocks */}
       <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
         {/* Heading */}
         <h2 className="text-4xl font-bold text-center">
-          What's Different About Manage?
+          Hear from some of our users
         </h2>
         {/* Testimonials Container */}
         <div className="flex flex-col mt-24 md:flex-row md:space-x-6">
           {/* Testimonial 1 */}
-          <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
-            <img src={avatarAnisha} className="w-16 -mt-14" alt="" />
-            <h5 className="text-lg font-bold">Anisha Li</h5>
-            <p className="text-sm text-darkGrayishBlue">
-              “Manage has supercharged our team's workflow. The ability to
-              maintain visibility on larger milestones at all times keeps
-              everyone motivated.”
-            </p>
-          </div>
-
-          {/* Testimonial 2 */}
-          <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
-            <img src={avatarAli} className="w-16 -mt-14" alt="" />
-            <h5 className="text-lg font-bold">Ali Bravo</h5>
-            <p className="text-sm text-darkGrayishBlue">
-              “We have been able to cancel so many other subscriptions since
-              using Manage. There is no more cross-channel confusion and
-              everyone is much more focused.”
-            </p>
-          </div>
-
-          {/* Testimonial 3 */}
-          <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:flex md:w-1/3">
-            <img src={avatarRichard} className="w-16 -mt-14" alt="" />
-            <h5 className="text-lg font-bold">Richard Watts</h5>
-            <p className="text-sm text-darkGrayishBlue">
-              “Manage has supercharged our team's workflow. The ability to
-              maintain visibility on larger milestones at all times keeps
-              everyone motivated.”
-            </p>
-          </div>
+          {testimonialArray.map((point, index) => (
+            <motion.div 
+            variants={fadeIn("left", "spring", 1, 0.3)}
+            initial="hidden"
+            whileInView="show"
+            id="services"
+            className="flex  shadow-grey shadow-md flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
+              <img src={point.image} className="w-16 -mt-14 rounded-full" alt="" />
+              <h5 className="text-lg font-bold">{point.name}</h5>
+              <p className="text-sm text-darkGrayishBlue">
+                {point.content}
+              </p>
+            </motion.div>
+          ))}
+              
         </div>
         {/* Button */}
         <div className="my-16">
